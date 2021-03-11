0f7924dcad2838108a74d3673ac127db50a57613
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-03-11 13:42:20 +0100

    mysql advanced 1/1, 1/2, 2/1.

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,17 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (5, "Bob", "Ross", "0D", "1234verzinwatstraat", "1234AB", "2003-07-23", "Amerika");
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (6, "Miracle", "Jhonson", "0A", "Maharhia23", "4334AB", "1987-12-02", "Kamarucho");
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (7, "Kazuma", "Kiryu", "0D", "Shifuku street 12", "1329CF", "1970-12-02", "Kamarucho");
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (8, "Elon", "Musk", "0B", "Mars", "8475MR", "2000-01-05", "Mars");
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (9, "Bob", "Aardappel", "0C", "Kerkstraat 56", "0231FG", "2001-11-16", "Groningen");
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (10, "Karel", "de Grote", "0A", "barteljorisstraat 4", "4567VC", "2003-05-22", "Amsterdam");
--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (voornaam, achternaam, woonplaats)
+VALUES ("Jhon", "souls", "Aalsmeer");
 
+INSERT INTO studenten (voornaam, achternaam, woonplaats)
+VALUES ("Pieter", "Post", "Amsterdam");
+
+INSERT INTO studenten (voornaam, achternaam, woonplaats)
+VALUES ("Peter", "Limonade", "Zwolle");
